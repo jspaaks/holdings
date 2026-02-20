ad5cd6ef8b03cc2c3319f8bcb15a64dd846fd4e5
Jurriaan H. Spaaks <jhspaaks@hotmail.com>
2026-02-20 11:34:58 -0500

    can now retrieve previous day's closing prices from api.massive.com, and updating the database / jsonserver file

--- backend/src/holdings/get_prices.py
+++ backend/src/holdings/get_prices.py
@@ -1,8 +1,17 @@
-from datetime import date, timedelta
-import json
+from datetime import datetime
 import os
 import sys
 from typing import List
+import requests
+from tenacity import retry, stop_after_attempt, wait_fixed, retry_if_result
+
+
+def is_too_many_requests(response):
+    too_many_requests = isinstance(response, requests.Response) and response.status_code == 429
+    if too_many_requests:
+        print(f'{datetime.now()} -- Too many requests, trying again in a moment')
+    return too_many_requests
+
 
 def get_apikey_from_env():
     name = 'API_KEY_MASSIVE'
@@ -15,73 +24,58 @@ def get_apikey_from_env():
 
 
 def get_symbols(url: str) -> List[str]:
-    mocked_holdings = [
-        {
-            'id': 'VOO',
-            'previous_close': {
-                'price': 22.81,
-                't': 1770930000
-            },
-            'transactions': [],
-        },
-        {
-            'id': 'RSP',
-            'previous_close': None,
-            'transactions': [],
-        },
-        {
-            'id': 'EMBJ',
-            'previous_close': {
-                'price': 125.01,
-                't': 1750930000
-            },
-            'transactions': [],
-        }
-    ]
-    return [h['id'] for h in mocked_holdings if h['previous_close'] is None or h['previous_close']['t'] < 1770930000]
+    response = requests.get(f'{url}/holdings')
+    response.raise_for_status()
+    holdings = response.json()
+    return [h['id'] for h in holdings if h['previous_close'] is None]
 
-def get_last_trading_minute() -> tuple[a,b]:
-    # use endpoint 'https://api.massive.com/v2/aggs/ticker/AAPL/range/1/day/2026-02-04/2026-02-18?adjusted=true&sort=asc&limit=15&apiKey=apikey'
-    today = date.today()
-    two_weeks_ago = today - timedelta(days=14)
-    apikey = get_apikey_from_env()
-    query = f'https://api.massive.com/v2/aggs/ticker/AAPL/range/1/day/{two_weeks_ago}/{today}?adjusted=true&sort=asc&limit=15&apiKey={apikey}'
-    print(query)
 
-    # use requests to do the query
-    # take results[resultCount-1].t, add 16*60*60*1000 milliseconds to get to 16:00 EST, call it 'end'
-    # subtract 59 * 1000 to get the minute before, call it 'start'
+def get_previous_close(symbol: str, apikey: str) -> dict[str, int]:
+    # https://massive.com/docs/rest/stocks/aggregates/previous-day-bar
+    print(f'{datetime.now()} -- Attempting to retrieve the previous closing price for {symbol}')
+    query = f'https://api.massive.com/v2/aggs/ticker/{symbol}/prev?adjusted=true&apiKey={apikey}'
+    response = make_throttled_request(query)
+    response.raise_for_status()
+    candlesticks = response.json()['results']
     return {
-        'start': 1771016340000,
-        'end': 1771016400000
+        't': candlesticks[-1]['t'],
+        'price': candlesticks[-1]['c']
     }
-    # query = f'https://api.massive.com/v2/aggs/ticker/AAPL/range/1/minute/{a}/{b}?adjusted=true&sort=asc&limit=1&apiKey={apikey}'
 
 
-def get_previous_close(symbol: str, apikey: str, t: int):
-    print('Attempting to GET previous close from api.massive.com')
+@retry(
+    stop=stop_after_attempt(2),
+    wait=wait_fixed(60),
+    retry=retry_if_result(is_too_many_requests)
+)
+def make_throttled_request(query):
+    return requests.get(query)
 
 
-def post_previous_close():
-    print('Attempting to POST previous close to cache')
+def post_previous_close(symbol: str, url: str, previous_close: dict[str, int]) -> None:
+    print(f'{datetime.now()} -- Attempting to update {symbol} with previous closing price')
+    response = requests.get(f'{url}/holdings/{symbol}')
+    response.raise_for_status()
+    holding = response.json()
+    holding.update({'previous_close': previous_close})
+    response = requests.patch(f'{url}/holdings/{symbol}', json=holding)
+    response.raise_for_status()
 
 
-def main():
-    if len(sys.argv) != 2:
-        print('Usage: python3 get_prices.py <URL>\n' +
+def cli():
+    if len(sys.argv) != 2 or sys.argv[1] in ['-h', '--help']:
+        print('Usage: get-prices <URL>\n' +
               '\n' +
               '   <URL>     URL for the holdings database server')
         return
 
-    URL = sys.argv[1]
+    url = sys.argv[1]
+    apikey = get_apikey_from_env()
 
-    minute = get_last_trading_minute()
-
-    for symbol in get_symbols(URL):
-        # get_previous_close(symbol, APIKEY)
-        # post_previous_close(symbol, price, URL)
-        print(symbol)
+    for symbol in get_symbols(url):
+        previous_close = get_previous_close(symbol, apikey)
+        post_previous_close(symbol, url, previous_close)
 
 
 if __name__ == '__main__':
-    main()
+    cli()
