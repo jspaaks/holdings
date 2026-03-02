ee25028ba43927aa2e1874343c6c6dc410eec9e3
Jurriaan H. Spaaks <jhspaaks@hotmail.com>
2026-03-02 09:12:58 -0500

    now doing the processing based on the cost basis file so the order of lots is correct

--- backend/src/holdings/process.py
+++ backend/src/holdings/process.py
@@ -8,22 +8,20 @@ import pandas as pd
 class Holdings:
 
     def __init__(self, input_filepath: str | os.PathLike):
-        self._skiprows = None
+        self._skiprows = 2
         self._sheet = None
         self._holdings = None
-        self._net_invested = None
 
-        self._calc_skiprows(input_filepath)
         self._read_from_csv(input_filepath)
         self._rename_sheet_column_labels()
-        self._calc_total_funds_received()
-        self._calc_holdings()
+        self._group_by_ticker()
+        self._reformat_dates()
         self._sort_holdings_by_date()
-        self._remove_holdings_fifo()
+
         self._calc_cost()
         self._calc_shares()
-        self._calc_shares_acc()
         self._calc_cost_per_share()
+        self._calc_shares_acc()
         self._calc_min_price()
         self._calc_max_price()
         self._add_previous_close_placeholder()
@@ -32,7 +30,6 @@ class Holdings:
         return json.dumps({
             '$schema': 'schema.json',
             'holdings': self._holdings,
-            'net_invested': self._net_invested
         }, sort_keys=True, indent=4)
 
     def _add_previous_close_placeholder(self):
@@ -42,123 +39,77 @@ class Holdings:
 
     def _calc_cost(self):
         for holding in self._holdings:
-            holding['cost'] = sum([t['price'] * t['shares'] for t in holding['buys']])
+            holding['cost'] = sum([ell['price'] * ell['shares'] for ell in holding['lots']])
         return self
 
     def _calc_cost_per_share(self):
         for holding in self._holdings:
             holding['cost_per_share'] = holding['cost'] / holding['shares']
+        return self
 
     def _calc_max_price(self):
         for holding in self._holdings:
-            holding['max_price'] = max([t['price'] for t in holding['buys']])
+            holding['max_price'] = max([ell['price'] for ell in holding['lots']])
 
     def _calc_min_price(self):
         for holding in self._holdings:
-            holding['min_price'] = min([t['price'] for t in holding['buys']])
+            holding['min_price'] = min([ell['price'] for ell in holding['lots']])
 
     def _calc_shares(self):
         for holding in self._holdings:
-            holding['shares'] = sum([t['shares'] for t in holding['buys']])
+            holding['shares'] = sum([ell['shares'] for ell in holding['lots']])
 
     def _calc_shares_acc(self):
         for holding in self._holdings:
             acc = 0
-            for b in holding['buys']:
-                b['shares_acc'] = acc
-                acc += b['shares']
+            for ell in holding['lots']:
+                ell['shares_acc'] = acc
+                acc += ell['shares']
 
-    def _calc_skiprows(self, input_filepath):
-        self._skiprows = 0
-        with open(input_filepath, 'r') as fid:
-            for line in fid:
-                if line == '\n':
-                    break
-                self._skiprows += 1
-            for line in fid:
-                if line != '\n':
-                    break
-                self._skiprows += 1
+    def _group_by_ticker(self):
+        # group by ticker symbol and wrangle transactions into an array
+        # of dict where key `id` represents ticker symbol
+        self._holdings = []
 
-    def _calc_total_funds_received(self):
-        selected = self._sheet.query('`type` == "Funds Received" or `type` == "Funds Transferred Out"')
-        self._net_invested = selected['amount'].sum()
+        # select only those rows that represent a buy or a sell and group them by ticker symbol
+        grouped = self._sheet.groupby('ticker')
+
+        for ticker, group in grouped:
+            lots = group.to_dict(orient='records')
+            for index, ell in enumerate(lots):
+                del ell['ticker']
+                ell['order'] = index
+            self._holdings.append({
+                'id': ticker,
+                'lots': lots
+            })
 
     def _read_from_csv(self, filepath: str | os.PathLike):
         filepath = Path(filepath)
         if not filepath.is_file():
             raise FileNotFoundError(f"File not found: '{filepath}'")
 
-        usecols = ['Trade Date', 'Transaction Type', 'Symbol', 'Shares', 'Share Price', 'Net Amount']
+        usecols = ['Acquired date', 'Symbol/CUSIP', 'Quantity', 'Cost per share']
         self._sheet = pd.read_csv(filepath, usecols=usecols, skiprows=self._skiprows)
+
+    def _reformat_dates(self):
+        for holding in self._holdings:
+            for ell in holding['lots']:
+                month, day, year = ell['date'].split('/')
+                ell['date'] = f'{year}-{month}-{day}'
 
     def _rename_sheet_column_labels(self):
         columns = {
-            'Trade Date': 'date',
-            'Transaction Type': 'type',
-            'Symbol': 'ticker',
-            'Shares': 'shares',
-            'Share Price': 'price',
-            'Net Amount': 'amount',
+            'Acquired date': 'date',
+            'Symbol/CUSIP': 'ticker',
+            'Quantity': 'shares',
+            'Cost per share': 'price',
         }
         self._sheet.rename(columns=columns, inplace=True)
 
-    def _calc_holdings(self):
-        # group by ticker symbol and wrangle transactions into an array
-        # of dict where key `id` represents ticker symbol
-        self._holdings = []
-
-        # select only those rows that represent a buy or a sell and group them by ticker symbol
-        grouped = self._sheet\
-            .query('`type` == "Buy" or `type` == "Sell"')\
-            .groupby('ticker')
-
-        for index, (ticker, group) in enumerate(grouped):
-            transactions = group.to_dict(orient='records')
-            for t in transactions:
-                del t['ticker']
-                t['type'] = t['type'].upper()
-                if t['type'] == 'SELL':
-                    t['shares'] *= -1
-            self._holdings.append({
-                'id': ticker,
-                'transactions': transactions
-            })
-
-    def _remove_holdings_fifo(self):
-        for holding in self._holdings:
-            shares_sold = sum(t['shares'] for t in holding['transactions'] if t['type'] == 'SELL')
-            if shares_sold == 0:
-                continue
-            for t in holding['transactions']:
-                if t['type'] != 'BUY':
-                    continue
-                delta = min(t['shares'], shares_sold)
-                t['shares'] -= delta
-                shares_sold -= delta
-                if shares_sold <= 0:
-                    break
-
-            # remove zero-share transactions
-            holding['transactions'] = list(filter(lambda t: t['shares'] > 0, holding['transactions']))
-
-            # remove SELLs from transactions
-            holding['transactions'] = list(filter(lambda t: t['type'] == 'BUY', holding['transactions']))
-
-        for holding in self._holdings:
-            # rename 'transactions' to 'buys'
-            holding['buys'] = holding.pop('transactions')
-
-            # remove the 'type' key from each transaction
-            for t in holding['buys']:
-                del t['type']
-
-        # remove holdings that have no buys
-        self._holdings = list(filter(lambda h: h['buys'], self._holdings))
-
     def _sort_holdings_by_date(self):
         for holding in self._holdings:
-            holding['transactions'].sort(key=lambda t: t['date'])
+            holding['lots'].sort(key=lambda ell: (ell['date'], ell['order']))
         return self
 
 
